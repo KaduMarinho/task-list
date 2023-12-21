79749e12360bfbb96e6377b9cb3c0714c952780a
Carlos.Gomes <carlos.gomes@cinbesa.com.br>
2023-12-21 17:15:39 -0300

    add and delete functionalities

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Task;
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +14,48 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+/**
+ * Show Task Dashboard
+ */
+Route::get('/', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+ //echo(date('h:i:s'));exit;
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+/**
+ * Add New Task
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+ 
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+ 
+    // Create The Task
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+ 
+    return redirect('/');
+});
+ 
+/**
+ * Delete Task
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    $task->delete();
+ 
+    return redirect('/');
 });
